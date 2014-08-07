379ef17f25bb5484f94fe5382db37bd0bb52153e
Suhayb El Wardany <me@suw.me>
2014-08-07 20:29:19 +0300

    validator updates
    Signed-off-by: Suhayb El Wardany <me@suw.me>

--- src/Traits/ValidatorTrait.php
+++ src/Traits/ValidatorTrait.php
@@ -17,108 +17,22 @@
  * @link       http://cartalyst.com
  */
 
-use Illuminate\Support\Fluent;
-use Illuminate\Support\Facades\Validator;
+use Cartalyst\Support\Validator;
 
 trait ValidatorTrait {
 
-	/**
-	 * The registered scenario.
-	 *
-	 * @var array
-	 */
-	protected $scenario = [];
+	protected $validator;
 
-	/**
-	 * The registered bindings
-	 *
-	 * @var array
-	 */
-	protected $bindings = [];
-
-	/**
-	 * Create a scope scenario.
-	 *
-	 * @param  string  $scenario
-	 * @param  array  $arguments
-	 * @return $this
-	 */
-	public function onScenario($scenario, array $arguments = [])
+	public function setValidator(Validator $validator)
 	{
-		$method = 'on'.ucfirst($scenario);
-
-		$this->scenario = [
-			'on'        => method_exists($this, $method) ? $method : null,
-			'arguments' => $arguments,
-		];
+		$this->validator = $validator;
 
 		return $this;
 	}
 
-	/**
-	 * Register the bindings.
-	 *
-	 * @param  array  $bindings
-	 * @return $this
-	 */
-	public function registerBindings(array $bindings)
+	public function getValidator()
 	{
-		$this->bindings = array_merge($this->bindings, $bindings);
-
-		return $this;
-	}
-
-	/**
-	 * Executes the data validation against the service rules.
-	 *
-	 * @param  array  $data
-	 * @return \Illuminate\Validation\Validator
-	 */
-	public function executeValidation(array $data)
-	{
-		if (is_null($this->scenario))
-		{
-			$this->onScenario('any');
-		}
-
-		if ($method = array_get($this->scenario, 'on', null))
-		{
-			call_user_func_array([$this, $method], $this->scenario['arguments']);
-		}
-
-		$rules = (new Fluent($this->getBindedRules()))->getAttributes();
-
-		return Validator::make($data, $rules);
-	}
-
-	/**
-	 * Returns the validation rules.
-	 *
-	 * @return array
-	 */
-	public function getRules()
-	{
-		return [];
-	}
-
-	/**
-	 * Returns the rules already binded.
-	 *
-	 * @return array
-	 */
-	protected function getBindedRules()
-	{
-		$rules = $this->getRules();
-
-		foreach ($rules as $key => $value)
-		{
-			if ($binding = array_get($this->bindings, $key, null))
-			{
-				$rules[$key] = str_replace('{'.$key.'}', $binding, $value);
-			}
-		}
-
-		return $rules;
+		return $this->validator;
 	}
 
 }
--- src/Validator.php
+++ src/Validator.php
@@ -17,12 +17,26 @@
  * @link       http://cartalyst.com
  */
 
-use Cartalyst\Support\Traits\ValidatorTrait;
 use Cartalyst\Support\Contracts\ValidatorInterface;
+use Cartalyst\Support\Traits\ValidatorTrait;
+use Illuminate\Support\Facades\Validator as V;
+use Illuminate\Support\Fluent;
 
 abstract class Validator implements ValidatorInterface {
 
-	use ValidatorTrait;
+	/**
+	 * The registered scenario.
+	 *
+	 * @var array
+	 */
+	protected $scenario = [];
+
+	/**
+	 * The registered bindings
+	 *
+	 * @var array
+	 */
+	protected $bindings = [];
 
 	/**
 	 * The validation rules.
@@ -61,6 +75,81 @@ abstract class Validator implements ValidatorInterface {
 	public function getRules()
 	{
 		return $this->rules;
+	}
+
+	/**
+	 * Create a scope scenario.
+	 *
+	 * @param  string  $scenario
+	 * @param  array  $arguments
+	 * @return $this
+	 */
+	public function onScenario($scenario, array $arguments = [])
+	{
+		$method = 'on'.ucfirst($scenario);
+
+		$this->scenario = [
+			'on'        => method_exists($this, $method) ? $method : null,
+			'arguments' => $arguments,
+		];
+
+		return $this;
+	}
+
+	/**
+	 * Register the bindings.
+	 *
+	 * @param  array  $bindings
+	 * @return $this
+	 */
+	public function registerBindings(array $bindings)
+	{
+		$this->bindings = array_merge($this->bindings, $bindings);
+
+		return $this;
+	}
+
+	/**
+	 * Executes the data validation against the service rules.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Validation\Validator
+	 */
+	public function executeValidation(array $data)
+	{
+		if (is_null($this->scenario))
+		{
+			$this->onScenario('any');
+		}
+
+		if ($method = array_get($this->scenario, 'on', null))
+		{
+			call_user_func_array([$this, $method], $this->scenario['arguments']);
+		}
+
+		$rules = (new Fluent($this->getBoundRules()))->getAttributes();
+
+		return V::make($data, $rules);
+	}
+
+	/**
+	 * Returns the rules already binded.
+	 *
+	 * @return array
+	 */
+	protected function getBoundRules()
+	{
+		$rules = $this->getRules();
+
+		foreach ($rules as $key => $value)
+		{
+			if ($binding = array_get($this->bindings, $key, null))
+			{
+				$rules[$key] = str_replace('{'.$key.'}', $binding, $value);
+			}
+		}
+
+		return $rules;
 	}
 
 }
