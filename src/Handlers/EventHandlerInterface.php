8921426b3cd250df04a7d21088c9eadda992814a
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-10-03 21:24:29 +0100

    added a event handler interface
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Handlers/EventHandlerInterface.php
@@ -0,0 +1,32 @@
+<?php namespace Cartalyst\Support\Handlers;
+/**
+ * Part of the Support package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
+ *
+ * @package    Support
+ * @version    1.1.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use Illuminate\Events\Dispatcher;
+
+interface EventHandlerInterface {
+
+	/**
+	 * Registers the event listeners using the given dispatcher instance.
+	 *
+	 * @param  \Illuminate\Events\Dispatcher  $dispatcher
+	 * @return void
+	 */
+	public function subscribe(Dispatcher $dispatcher);
+
+}
