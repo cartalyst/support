6bfbf372311ca047eb18eea45e7088ff3b4949f3
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-11-01 02:41:00 +0000

    add event handler test and update phpunit.xml
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ tests/Handlers/EventHandlerTest.php
@@ -0,0 +1,47 @@
+<?php namespace Cartalyst\Support\Tests\Handlers;
+/**
+ * Part of the Support package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
+ *
+ * @package    Support
+ * @version    1.1.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use PHPUnit_Framework_TestCase;
+use Illuminate\Container\Container;
+use Cartalyst\Support\Handlers\EventHandler;
+
+class EventHandlerTest extends PHPUnit_Framework_TestCase {
+
+	/** @test **/
+	public function it_can_be_instantiated()
+	{
+		new EventHandlerStub(new Container);
+	}
+
+	/** @test */
+	public function it_can_retrieve_dynamic_objects_from_the_container()
+	{
+		$container = new Container;
+		$container->bind('foo', function(){ return 'bar'; });
+
+		$handler = new EventHandlerStub($container);
+
+		$this->assertSame('bar', $handler->foo);
+	}
+
+}
+
+class EventHandlerStub extends EventHandler {
+
+}
