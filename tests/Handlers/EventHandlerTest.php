b8dee6af39ddd0d6473140e5fe4f9d64169ac89a
Suhayb Wardany <me@suw.me>
2022-02-11 12:49:31 +0200

    chore: bump version
    Signed-off-by: Suhayb Wardany <me@suw.me>

--- tests/Handlers/EventHandlerTest.php
+++ tests/Handlers/EventHandlerTest.php
@@ -11,10 +11,10 @@
  * bundled with this package in the LICENSE file.
  *
  * @package    Support
- * @version    5.1.2
+ * @version    6.0.0
  * @author     Cartalyst LLC
  * @license    BSD License (3-clause)
- * @copyright  (c) 2011-2021, Cartalyst LLC
+ * @copyright  (c) 2011-2022, Cartalyst LLC
  * @link       https://cartalyst.com
  */
 
